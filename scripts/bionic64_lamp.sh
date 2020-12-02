5e49ac0ce652f6644951f3309ceb2d74e308fe1c
James Roi Dela Cruz <75005799+AzraelHive@users.noreply.github.com>
2020-12-02 22:27:39 +0800

    Create bionic64_lamp.sh

---
+++ scripts/bionic64_lamp.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+##
+## LAMP Stack Installation Script (Bionic64)
+## Copyright © 2020 AzraelHive
+##
+
+## Adding Apache2 (2.4.46) to Repository
+sudo add-apt-repository ppa:ondrej/apache2
+
+## Update and Upgrade Distro
+sudo apt update
+sudo apt upgrade -y 
+
+## Install Git, Curl, Unzip and Build-Essential
+sudo apt install -y unzip curl git 
+
+## Install Base Components
+sudo apt install -y build-essential libapr1-dev libaprutil1-dev libpcre3-dev
+
+## Install & Setup Web Server 
+sudo apt install apache2 -y
+sudo cd /etc/apache2/sites-available
+sudo rm 000-default.conf
+sudo wget https://raw.githubusercontent.com/AzraelHive/aziops/main/scripts/stacks/etc/apache2/sites-available/000-default.conf -O /etc/apache2/sites-available/000-default.conf
+sudo a2enmod rewrite
+
+## Install & Setup MySQL Database
+sudo apt install mysql-server mysql-client -y 
+sudo mysql -u root -p <<myQuery
+  DROP USER ''@'localhost'
+  DROP USER ''@'$(hostname)'
+  DROP DATABASE test
+  FLUSH PRIVILEGES;
+myQuery
+
+## Install & Setup PHP
+sudo apt install php7.2 -y
+sudo apt install php7.2-bcmath php7.2-curl php7.2-dev php7.2-gd php7.2-intl php7.2-mbstring php7.2-xml php7.2-zip php7.2-mysql php-cli libapache2-mod-php php7.2-dev -y
+
+## Create PHP Information File
+sudo echo "<?php phpinfo();?>" >> /var/www/html/info.php
+
+## Install & Setup Composer
+sudo curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo rm composer-setup.php
